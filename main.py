fdc577fe362ee5cae94be12d972527cbc4cf7bd4
krptstc <krptstc@seznam.cz>
2021-05-05 11:25:04 +0200

    Getting Numbers from the Input

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from modules.funkce       import *
-from modules.molhmotnosti import *
 
 if __name__ == '__main__':
     zvolenaSloucenina = input("Zadejte sloučeninu: ")
     prvkySlouceniny   = ziskat_prvky(zvolenaSloucenina)
-    print("\nPrvky této sloučeniny:")
+    molarniHmotnost   = ziskat_molarni_hmotnost(zvolenaSloucenina)
+    print('\nPrvky této sloučeniny:')
     print(prvkySlouceniny)
+    print(f'Molární hmotnost sloučeniny: {molarniHmotnost} g/mol')
--- modules/funkce.py
+++ modules/funkce.py
@@ -1,3 +1,5 @@
+from modules.molhmotnosti import *
+
 def ziskat_prvky(sloucenina):
     prvky = []
     momentalniPrvek = ''
@@ -6,6 +8,20 @@ def ziskat_prvky(sloucenina):
             if len(momentalniPrvek) != 0:
                 prvky.append(momentalniPrvek)
             momentalniPrvek = ''
-        momentalniPrvek += pismeno
-    prvky.append(momentalniPrvek)
+            momentalniPrvek += pismeno
+        elif pismeno.isnumeric():
+            for i in range(int(pismeno)):
+                prvky.append(momentalniPrvek)
+            momentalniPrvek = ''
+        else:
+            momentalniPrvek += pismeno
+    if not pismeno.isnumeric():
+        prvky.append(momentalniPrvek)
     return prvky
+
+def ziskat_molarni_hmotnost(sloucenina):
+    vysledek = 0
+    prvky    = ziskat_prvky(sloucenina)
+    for prvek in prvky:
+        vysledek += molarniHmotnost[prvek]
+    return vysledek
