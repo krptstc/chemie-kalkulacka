2d0636e37c2d83be2ec0c1b2f78a94df7c21eafc
krptstc <krptstc@seznam.cz>
2021-05-05 15:40:36 +0200

    Numbers Before Items

--- main.py
+++ main.py
@@ -9,13 +9,39 @@ if __name__ == '__main__':
     pravaStrana = ziskat_stranu()
 
     rovnice = levaStrana + pravaStrana
+    for sloucenina in rovnice:
+        sloucenina.replace(' ', '')
 
     vstupniLatka  = input('\nZadejte sloučeninu na vstupu: ')
+    vstupniLatka.replace(' ', '')
     vystupniLatka = input('Zadejte sloučeninu na výstupu: ')
+    vystupniLatka.replace(' ', '')
     poziceVstup   = rovnice.index(vstupniLatka)
     poziceVystup  = rovnice.index(vystupniLatka)
-    hodnotaVstup  = ziskat_molarni_hmotnost(rovnice[poziceVstup])
-    hodnotaVystup = ziskat_molarni_hmotnost(rovnice[poziceVystup])
+    hodnotaVstup  = 0
+    hodnotaVystup = 0
+
+    if rovnice[poziceVstup][0].isnumeric():
+        bezCisel = []
+        for c in rovnice[poziceVstup]:
+            if not c.isnumeric():
+                bezCisel.append(c)
+
+        hodnotaVstup  = ziskat_molarni_hmotnost(bezCisel)
+        hodnotaVstup *= int(rovnice[poziceVstup][0])
+    else:
+        hodnotaVstup  = ziskat_molarni_hmotnost(rovnice[poziceVstup])
+
+    if rovnice[poziceVystup][0].isnumeric():
+        bezCisel = []
+        for c in rovnice[poziceVystup]:
+            if not c.isnumeric():
+                bezCisel.append(c)
+
+        hodnotaVystup  = ziskat_molarni_hmotnost(bezCisel)
+        hodnotaVystup *= int(rovnice[poziceVystup][0])
+    else:
+        hodnotaVystup  = ziskat_molarni_hmotnost(rovnice[poziceVystup])
 
     print(f'\nKolik máme gramů vstupní látky?')
 
